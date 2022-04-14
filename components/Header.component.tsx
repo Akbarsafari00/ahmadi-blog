83f505d04fbdfcf63eed27a56d54283ab70c175b
akbarsafari00 <akbarsafari00@gmail.com>
2022-04-14 10:38:45 +0430

    init

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -3,35 +3,40 @@ import Head from "next/head";
 import styles from '../styles/Components/Header.module.scss'
 import Link from "next/link";
 import $ from 'jquery';
+import Image from 'next/image'
 
-function HeaderComponent({title}:{title: string}) {
+function HeaderComponent({title}: { title: string }) {
     return (
         <header className={styles.header}>
-           <div className={styles.header__container}>
-               <div className={styles.header__toggle} onClick={doClick}>
-                   <span></span>
-                   <span></span>
-                   <span></span>
-               </div>
-               <div className={styles.header__brand}>
-                   <h4 className={styles.title}>وبلاگ احمدی</h4>
-               </div>
-               <div className={styles.header__menu}>
-                   <ul className={styles.menu}>
-                       <li><Link href={"#"}>صفحه اصلی</Link></li>
-                       <li><Link href={"#"}>مقالات</Link></li>
-                       <li><Link href={"#"}>تماس با من</Link></li>
-                   </ul>
-               </div>
-               <div className={styles.header__action}>
-                   <Link  href={"#"}><span className={styles.login}>ورود</span></Link>
-               </div>
-           </div>
+            <div className={styles.header__container}>
+                <div className={styles.header__toggle} onClick={doClick}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className={styles.header__brand}>
+                    <h4 className={styles.title}>وبلاگ احمدی</h4>
+                </div>
+                <div className={styles.header__menu}>
+                    <ul className={styles.menu}>
+                        <li><Link href={"#"}>صفحه اصلی</Link></li>
+                        <li><Link href={"#"}>مقالات</Link></li>
+                        <li><Link href={"#"}>تماس با من</Link></li>
+                    </ul>
+                </div>
+                <div className={styles.header__action}>
+                    <Link href={"#"}><a className={styles.login}><Image src={"/svg/noun-user-4756261.svg"} width={24}
+                                                                        height={24}/></a></Link>
+                    <Link href={"#"}><a className={styles.search}><Image src={"/svg/noun-search-1868248.svg"} width={24}
+                                                                        height={24}/></a></Link>
+                </div>
+            </div>
         </header>
     );
 }
 
 function doClick() {
-    $("."+styles.header__menu).animate({width:'toggle'},200);
+    $("." + styles.header__menu).animate({width: 'toggle'}, 200);
 }
+
 export default HeaderComponent;
